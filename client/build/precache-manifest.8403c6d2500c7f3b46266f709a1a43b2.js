29b7b018eaccab4eb3ce6abbc54bfe7e93a19c33
Dennis Tran <dennistran714@gmail.com>
2020-04-08 19:42:43 -0700

    testing deployment

---
+++ client/build/precache-manifest.8403c6d2500c7f3b46266f709a1a43b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3658825562d89f63035c",
+    "url": "/static/js/main.4e357bd4.chunk.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "3658825562d89f63035c",
+    "url": "/static/css/main.aa0ca161.chunk.css"
+  },
+  {
+    "revision": "e33fc6cc53dc857275ef16bbf58c61a6",
+    "url": "/index.html"
+  }
+];
